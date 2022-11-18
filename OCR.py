2c5b13889b1f404ba5892727c43a5308285c64bb
UnStunned <kulkarnitheonly@gmail.com>
2022-11-18 10:15:20 +0530

    18-11-2022; After demonstration

--- OCR.py
+++ OCR.py
@@ -3,7 +3,7 @@ import pytesseract
 from pytesseract import Output
 import cv2 as cv
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\djsce.student\\AppData\\Local\\Tesseract-OCR\\tesseract.exe'
 VALID_CHARACTERS = 'a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9'
 VALID_CHARACTERS = VALID_CHARACTERS.upper()
 VALID_CHARACTERS = VALID_CHARACTERS.split(" ")
@@ -28,13 +28,17 @@ for i in os.listdir('processed_images_after_thresholding'):
     for _ in seperated_text:
         if _ not in VALID_CHARACTERS:
             seperated_text.remove(_)
+    
+    for _ in seperated_text:
+        if _ in [".", "!", "|", "<", ","]:
+            seperated_text.remove(_)
 
     flattened_text = ''
     for _ in seperated_text:
         flattened_text = flattened_text + _
 
     with open('Results.txt','a') as f:
-        if len(flattened_text) > 3:
+        if len(flattened_text) > 4:
             f.write(f'{car_name} : ' + flattened_text + '\n')
 
 
